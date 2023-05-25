7f96cad493b63aabadb7cc61f02fb33f126431b5
nazibur-175 <nazibur175@gmail.com>
2023-05-26 04:52:14 +0600

    first commit

---
+++ src/mycomponents/Class.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Class extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello I am Nazibur Rahman .</h1>
+      </div>
+    );
+  }
+}
+
+export default Class;
---
+++ src/mycomponents/Function.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Function() {
+  return (
+    <div>
+      <h1>Hlw World</h1>
+    </div>
+  );
+}
+
+export default Function;
